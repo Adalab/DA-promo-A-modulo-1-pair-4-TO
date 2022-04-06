59a820e7bb5f022335f33b0a8f6f1f558a5b8e6c
Olivia <oliviamarino06@gmail.com>
2022-04-06 12:28:42 +0200

    actualizamos pairs sql

---
+++ SQL/ejercicio_pair_programming_modificar_tablas_1.sql
@@ -0,0 +1,14 @@
+-- Ejercicio 1:
+
+ALTER TABLE zapatillas ADD COLUMN marca VARCHAR(45) NOT NULL;
+
+-- Ejercicio 2: No hay nada que cambiar. Salario ya es FLOAT.
+
+-- Ejercicio 3:
+
+ALTER TABLE clientes DROP COLUMN pais;
+-- CP: No hay nada que cambiar, ya es un INT(5).
+
+
+-- Ejercicio 4: No hay nada que cambiar. Ya tenemos columna de TOTAL.
+
