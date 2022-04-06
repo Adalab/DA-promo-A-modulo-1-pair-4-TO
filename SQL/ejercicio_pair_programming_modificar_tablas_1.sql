363ddaf3c58daf81e3578e751235795a14931579
Olivia <oliviamarino06@gmail.com>
2022-04-06 13:02:42 +0200

    actualizo pair 13-modificar-tablas-1

--- SQL/ejercicio_pair_programming_modificar_tablas_1.sql
+++ SQL/ejercicio_pair_programming_modificar_tablas_1.sql
@@ -2,13 +2,15 @@
 
 ALTER TABLE zapatillas ADD COLUMN marca VARCHAR(45) NOT NULL;
 
--- Ejercicio 2: No hay nada que cambiar. Salario ya es FLOAT.
+-- Ejercicio 2:
+ALTER TABLE empleados MODIFY salario FLOAT NOT NULL;
 
 -- Ejercicio 3:
 
 ALTER TABLE clientes DROP COLUMN pais;
--- CP: No hay nada que cambiar, ya es un INT(5).
+ALTER TABLE clientes MODIFY codigo_postal INT(5);
 
 
--- Ejercicio 4: No hay nada que cambiar. Ya tenemos columna de TOTAL.
+-- Ejercicio 4: 
+ALTER TABLE facturas ADD COLUMN total FLOAT DEFAULT NULL;
 
